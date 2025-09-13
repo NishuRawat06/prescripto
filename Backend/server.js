69c9b68b0cef997f6a273a8e2c3d2de70c02c27b
NishuRawat06 <rawatnishu2005@gmail.com>
2025-09-13 18:26:29 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
-const express =require('express');
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
 const app=express();
-const cors = require("cors");
-const path=require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -11,4 +11,4 @@ app.get("/",(req,res)=>{
     res.send("backend is working");
 });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log('server is running on port ${PORT}'));
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`));
