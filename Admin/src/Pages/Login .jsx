b101d76f21d508297c0e38d182b0bd8c4a35bce9
NishuRawat06 <rawatnishu2005@gmail.com>
2025-09-20 15:01:11 +0530

    update

--- Admin/src/Pages/Login .jsx
+++ Admin/src/Pages/Login .jsx
@@ -1,18 +1,39 @@
-import React,{useState} from "react";
-// import {assets} from '../assets/assets'
+import React,{useContext, useState} from "react";
+// import {assets} from '../assets/assets';
+import { Admincontext } from "../Context/AdminContext";
+import axios from 'axios'
 export default function Login(){
     const [state,setState]=useState('Admin')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const {SetAToken,backendurl}=useContext(Admincontext)
+    const onsubmithandler =async (event)=>{
+        event.preventDefault();
+        try {
+          if (state === "Admin") {
+            const { data } = await axios.post(backendurl + "/api/admin/login", {
+              email,
+              password,
+            });
+            if (data.success) {
+              localStorage.setItem("aToken", data.token);
+              SetAToken(data.token);
+            }
+          } else {
+          }
+        } catch (error) {}
+    }
     return(
        <form className="min-h-[80vh] flex items-center">
         <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg ">
             <p className="text-2xl font-semibold m-auto"><span className="text-[#6677FF]">{state}</span>Login</p>
             <div className="w-full">
                 <p>Email</p>
-               <input className="border border-[#DADADA] rounded w-full p-2 mt-1" type='Email' required/>
+               <input onChange={(e)=>(setEmail(e.target.value))} value={email} className="border border-[#DADADA] rounded w-full p-2 mt-1" type='Email' required/>
             </div>
               <div className="w-full">
                 <p>Password</p>
-                <input className="border border-[#DADADA] rounded w-full p-2 mt-1" type='password' required/>
+                <input onChange={(e)=>(setPassword(e.target.value))} value={password} className="border border-[#DADADA] rounded w-full p-2 mt-1" type='password' required/>
             </div>
             <button className="bg-[#6677FF] text-white w-full py-2 rounded-md text-base">Login</button>
             {
