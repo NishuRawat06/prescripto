86cb6b44771dbee619635082049db9a52de66045
NishuRawat06 <rawatnishu2005@gmail.com>
2025-09-21 13:58:11 +0530

    update

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import Login from "./Pages/Login ";
-const App=()=>{
-return(
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { Admincontext } from "./Context/AdminContext";
+import Navbar from "./Component/Navbar";
+import Sidebar from "./Component/Sidebar";
+const App = () => {
+  const {aToken} = useContext(Admincontext)
+return aToken?(
+  <div>
+<ToastContainer/>
+<Navbar/>
+<div>
+  <Sidebar/>
+</div>
+  </div>
+) :(
   <div>
 <Login/>
+<ToastContainer/>
   </div>
 )
 }
--- Admin/src/Context/AdminContext.jsx
+++ Admin/src/Context/AdminContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 export const Admincontext=createContext();
 const AdminContextProvider =(props)=>{
-  const [aToken,SetAToken] =useState('')
+  const [aToken,SetAToken] =useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
   const backendurl=import.meta.env.VITE_BACKEND_URL
     const value ={
       aToken,SetAToken,
