191c4db77c6fcc3b56f031cc158641eb92f04b5f
NishuRawat06 <rawatnishu2005@gmail.com>
2025-10-04 13:31:39 +0530

    latest

--- Admin/src/Component/Navbar.jsx
+++ Admin/src/Component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React,{useContext} from "react";
-import assets from '../assets/assets.js';
+import admin_logo from "../assets/Admin_logo.svg";
 import {Admincontext} from '../Context/AdminContext';
 import {useNavigate} from 'react-router-dom'
 const Navbar =() =>{
@@ -13,7 +13,7 @@ const Navbar =() =>{
     return(
         <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
         <div className="flex items-center gap-2 text-xs">
-            <img className="w-36 sm:w-40 cursor-pointer h-12 " src={assets.admin_logo} alt=""/>
+            <img className="w-36 sm:w-40 cursor-pointer h-12 " src={admin_logo} alt=""/>
             <p className="boredr px-2.5 py-0.5 rounded-full boredr-grey-500 text-grey-600">{aToken ? 'Admin' :'Doctor'}</p>
         </div>
         <button onClick={logout} className="bg-[#6677FF] text-white text-sm px-10 py-2 rounded-full">Logout</button>
